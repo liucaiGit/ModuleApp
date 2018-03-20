7b2ecbe77328ac8dfc21913c9f687e2b483ae7f9
liucai222 <liucai@HuiSouMacBook-Pro.local>
2018-03-20 19:52:50 +0800

    no message

---
+++ DataCache/DataCache.m
@@ -0,0 +1,28 @@
+//
+//  DataCache.m
+//  CloneFactoryApp
+//
+//  Created by liucai on 2017/9/4.
+//  Copyright © 2017年 CSCHS. All rights reserved.
+//
+
+#import "DataCache.h"
+
+@implementation DataCache
+
++ (BOOL)keyedArchiverWithData:(id)datas path:(NSString *)path {
+    //拼接存储地址
+    NSString *filePath = [NSHomeDirectory() stringByAppendingPathComponent:[NSString stringWithFormat:@"Documents/%@",path]];
+        
+    BOOL flag = [NSKeyedArchiver archiveRootObject:datas toFile:filePath];
+    return flag;
+}
+
++ (id)keyedUnArchiverWithPath:(NSString *)path {
+    //拼接存储地址
+    NSString *filePath = [NSHomeDirectory() stringByAppendingPathComponent:[NSString stringWithFormat:@"Documents/%@",path]];
+    id data = [NSKeyedUnarchiver unarchiveObjectWithFile:filePath];
+    return data;
+}
+
+@end
